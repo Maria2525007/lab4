75eecc5fa2c516a739aab5b18c0217ed31aaa70c
Maria2525007 <maria-06112004@mail.ru>
2023-02-18 11:48:02 +0300

    Initial commit

--- src/Main2.java
+++ src/Main2.java
@@ -1,11 +1,17 @@
 
 class C1{
     private final Integer i;
+    public int b;
 
     C1(Integer i) {
         this.i = i;
     }
 
+    @Override
+    public int hashCode(Object a) {
+        this.b=b;
+        return (b);
+    }
 }
 
 public class Main2 {
@@ -13,12 +19,11 @@ public class Main2 {
         var i1 = new C1(1);
         var i2 = new C1(1);
 
-
-
-        System.out.println(i1.equals(i2));
-       // System.out.println(i1.equals(null));
+        System.out.println(i1);
+        System.out.println(i2);
         System.out.println(i1.hashCode());
         System.out.println(i2.hashCode());
+        System.out.println(i1.equals(i2));
 
     }
 }
